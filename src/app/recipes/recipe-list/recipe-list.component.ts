96ac62359f3f791e4e552af6df83ae38403d4db8
Manoj Parmar <manoj.parmar@forusall.com>
2022-04-16 14:14:50 +0530

    created all the required components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Double cheez pizza', 'https://image.shutterstock.com/image-photo/kiev-ukraine-031219-sign-dominos-600w-1347282647.jpg'),
+    new Recipe('Pizza', 'Double cheez pizza', 'https://image.shutterstock.com/image-photo/kiev-ukraine-031219-sign-dominos-600w-1347282647.jpg')
+  ]
   constructor() { }
 
   ngOnInit(): void {
